82031effe4f2cf9fd77adfe55e33151120f242fc
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2024-03-08 20:47:47 -0600

    add elements list

--- src/components/Items/Items.tsx
+++ src/components/Items/Items.tsx
@@ -1,18 +1,50 @@
 import items from "./../GameStateProvider/items.json";
 import { InventorySlot } from "../InventorySlot/InventorySlot";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { GameStateContext } from "../GameStateProvider/GameStateProvider";
 
 export const Items = () => {
   const { getQuantity } = useContext(GameStateContext);
+  const [curElement, setCurElement] = useState<string>("");
+
+  const elements = Array.from(
+    Object.values(items).reduce((acc, item) => {
+      acc.add(item.element);
+      return acc;
+    }, new Set<string>())
+  ).sort();
 
   return (
     <>
       <h2 className="text-l">Inventory</h2>
       <div>
-        {Object.entries(items).map(([item, info]) => (
-          <InventorySlot key={item} item={item as keyof typeof items} info={info} />
+        <button
+          className="rounded border-solid border border-gray-800 bg-gray-200 hover:bg-gray-400 px-1 ml-4"
+          onClick={(e) => {
+            e.preventDefault();
+            setCurElement("");
+          }}
+        >
+          Show all
+        </button>
+        {elements.map((elem) => (
+          <button
+            className="rounded border-solid border border-gray-800 bg-gray-200 hover:bg-gray-400 px-1 ml-4"
+            onClick={(e) => {
+              e.preventDefault();
+              setCurElement(elem);
+            }}
+          >
+            {elem}
+          </button>
         ))}
+      </div>
+      <div>
+        {Object.entries(items)
+          .filter(([, info]) => curElement === "" || curElement === info.element)
+          .map(([item, info]) => (
+            <InventorySlot key={item} item={item as keyof typeof items} info={info} />
+          ))}
       </div>
       <div>
         <h2 className="text-l">Total weight</h2>
