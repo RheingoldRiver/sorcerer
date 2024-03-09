dbbf057c26fa3b5e33aa080d3fb87683f089d7c1
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2024-03-08 20:37:07 -0600

    display weight

--- src/components/Items/Items.tsx
+++ src/components/Items/Items.tsx
@@ -1,12 +1,25 @@
 import items from "./../GameStateProvider/items.json";
 import { InventorySlot } from "../InventorySlot/InventorySlot";
+import { useContext } from "react";
+import { GameStateContext } from "../GameStateProvider/GameStateProvider";
 
 export const Items = () => {
+  const { getQuantity } = useContext(GameStateContext);
+
   return (
-    <div>
-      {Object.entries(items).map(([item, info]) => (
-        <InventorySlot key={item} item={item as keyof typeof items} info={info} />
-      ))}
-    </div>
+    <>
+      <h2 className="text-l">Inventory</h2>
+      <div>
+        {Object.entries(items).map(([item, info]) => (
+          <InventorySlot key={item} item={item as keyof typeof items} info={info} />
+        ))}
+      </div>
+      <div>
+        <h2 className="text-l">Total weight</h2>
+        {Object.entries(items).reduce((acc, [val, info]) => {
+          return getQuantity(val as keyof typeof items) * info.weight + acc;
+        }, 0)}
+      </div>
+    </>
   );
 };
